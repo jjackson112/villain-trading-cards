5634d3d76c0123f43e00a03b5e5c5cbec487d5d6
Jazz <jjackson11211@gmail.com>
2025-04-15 00:19:36 -0400

    set up sql database

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # Initialize the Flask application
 app = Flask(__name__)
+
+# set up SQL database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
