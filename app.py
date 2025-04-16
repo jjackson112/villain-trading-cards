226e673bc8395c9ad396b14e89edef9fdb5f2ae2
Jazz <jjackson11211@gmail.com>
2025-04-16 08:25:27 -0400

    Add villain to database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -10,7 +10,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
 db = SQLAlchemy(app)
 
 # organize SQL database
-class Villain (db.model):
+class Villain (db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), unique=True, nullable=False)
   description = db.Column(db.String(250), unique=True, nullable=False)
@@ -22,9 +22,52 @@ class Villain (db.model):
 def __repr__ (self):
   return "<Villain "+ self.name + ">"
 
+# connect sql database to web app
+with app.app_context():
+  db.create_all()
+  db.session.commit()
+
 @app.route("/")
-def hello_world():
+def villains_cards():
   return render_template("villain.html")
+
+# Add a villain to database
+# errors handles if the user doesn't fill out required fields
+@app.route("/add", methods=["GET"])
+def add_villain():
+  return render_template("addvillain.html", errors=[])
+
+# POST method to add villain
+# errors alerts users if they don't submit all required fields
+@app.route("/addVillain", methods=["POST"])
+def add_user():
+  errors = []
+# get new villain's name
+name = request.form.get("name")
+if not name:
+  errors.append("Oops! Looks like you forgot a name!")
+description = request.form.get("description")
+if not description:
+  errors.append("Oops! Looks like you forgot a description!")
+  interests = request.form.get("interests")
+if not interests:
+  errors.append("Oops! Looks like you forgot some interests!")
+url = request.form.get("url")
+if not url:
+  errors.append("Oops! Looks like you forgot an image!")
+
+# query villain database
+villain = Villain.query.filter_by(name=name).first()
+if villain:
+  errors.append("Oops! A villain with that name already exists!")
+
+if errors:
+  render_template("addvillain.html", errors=errors)
+else:
+  new_villain = Villain(name=name, description=description, interests=interests, url=url)
+  db.session.add(new_villain)
+  db.session.commit()
+  return render_template("villain.html", villains=Villain.query.all())
 
 # Run the flask server
 app.run(host="0.0.0.0", port=8080)
